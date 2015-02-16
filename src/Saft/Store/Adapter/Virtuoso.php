2907774b4d190be394c7c796f919aadee789bd5c
Konrad Abicht <k.abicht@googlemail.com>
2015-02-16 19:13:07 +0100

    Add Virtuoso store adapter from github:k00ni/Enable
    It provides full access to Virtuoso Server via ODBC using PDO

---
+++ src/Saft/Store/Adapter/Virtuoso.php
@@ -0,0 +1,435 @@
+<?php
+
+namespace Saft\Store\Adapter;
+
+class Virtuoso extends AbstractAdapter
+{
+    /**
+     * Adapter option array
+     * 
+     * @var array
+     */
+    protected $config = null;
+
+    /**
+     * PDO ODBC
+     * @var \PDO
+     */
+    protected $connect = null;
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        
+    }
+
+    /**
+     * Destructor
+     */
+    public function __destruct()
+    {
+        $this->disconnect();
+    }
+    
+    /**
+     * Add a new empty and named graph.
+     *
+     * @param string $graphUri URI of the graph to create
+     * @return void
+     * @throws \Exception
+     */
+    public function addGraph($graphUri)
+    {
+        $this->executeQuery("CREATE SILENT GRAPH <$graphUri>");
+    }
+    
+    /**
+     * Adds multiple triples to the graph.
+     * 
+     * @param string $graphUri URI of the graph to add the triples
+     * @param array $triples Array of triples to add.
+     * @return \PDOStatement
+     * @throw \Exception
+     */
+    public function addMultipleTriples($graphUri, array $triples)
+    {
+        $tripleNumber = count($triples);
+        
+        /**
+         * create batches
+         */ 
+        $batch = array();
+        $batchSize = 50;
+
+        for($i = 0; $i < $tripleNumber; ++$i) {
+            if (0 == $i % $batchSize) {
+
+                $this->sparql(
+                    "INSERT INTO GRAPH <". $graphUri ."> {" .
+                        \Saft\RDF\Triple::buildTripleString($batch) .
+                    "}"
+                );
+                 
+                $batch = array();
+            }
+            $batch[] = $triples[$i];
+        }
+        
+        $result = $this->sparql(
+            "INSERT INTO GRAPH <". $graphUri ."> {" .
+                \Saft\RDF\Triple::buildTripleString($batch) .
+            "}"
+        );
+
+        return $result;
+    }
+    
+    /**
+     * Add a triple.
+     * 
+     * @param string $graphUri URI of the graph to add triple
+     * @param string $subject URI of the subject to add
+     * @param string $predicate URI of the predicate to add
+     * @param array $object Array with data of the object to add
+     * @return ODBC resource
+     * @throw \Exception
+     */
+    public function addTriple($graphUri, $subject, $predicate, array $object)
+    {
+        return $this->addMultipleTriples(
+            $graphUri, array(array($subject, $predicate, $object))
+        );
+    }
+    
+    /**
+     * Checks that all requirements for Virtuoso via PDO-ODBC are fullfilled.
+     */
+    public function checkRequirements()
+    {
+        if (!extension_loaded("pdo_odbc")) {
+            throw new \Exception(
+                "Virtuoso adapter requires the PDO_ODBC extension to be loaded."
+            );
+        }
+        
+        return true;
+    }  
+    
+    /**
+     * Closes a current connection to the database.
+     * 
+     * @return void
+     */
+    public function disconnect()
+    {
+        $this->connect = null;
+    }
+    
+    /**
+     * Drops a graph.
+     * 
+     * @param string $graphUri URI of the graph to remove
+     * @throws \Exception
+     */
+    public function dropGraph($graphUri)
+    {
+        $this->sparql("DROP SILENT GRAPH <$graphUri>");
+    }
+    
+    /**
+     * Drops multiple triples.
+     * 
+     * @param string $graphUri URI of the graph to drop triples
+     * @param array $triples Array of triples to drop
+     * @return ODBC resource Last used ODBC resource
+     * @throws \Exception
+     */
+    public function dropMultipleTriples($graphUri, array $triples)
+    {        
+        $tripleNumber = count($triples);
+        
+        /**
+         * create batches
+         */ 
+        $batch = array();
+        $batchSize = 250;
+
+        for($i = 0; $i < $tripleNumber; ++$i) {
+            if (0 == $i % $batchSize) {
+
+                $odbcRes = $this->sparql(
+                    "DELETE FROM GRAPH <". $graphUri ."> {".
+                        \Saft\RDF\Triple::buildTripleString($batch) .
+                    "}"
+                );
+                 
+                $batch = array();
+            }
+            $batch[] = $triples[$i];
+        }
+        
+        $odbcRes = $this->sparql(
+            "DELETE FROM GRAPH <". $graphUri ."> {".
+                \Saft\RDF\Triple::buildTripleString($batch) .
+            "}"
+        );
+
+        return $odbcRes;
+    }
+    
+    /**
+     * Drops a triple.
+     * 
+     * @param string $graphUri URI of the graph to drop triple
+     * @param string $subject URI of the subject to drop
+     * @param string $predicate URI of the predicate to drop
+     * @param array $object Array with data of the object to drop
+     * @return ODBC resource
+     * @throw \Exception
+     */
+    public function dropTriple($graphUri, $subject, $predicate, array $object)
+    {
+        return $this->dropMultipleTriples(
+            $graphUri, array(array($subject, $predicate, $object))
+        );
+    }
+    
+    /**
+     * Executes a SPARQL and SQL query on the database. Using PDO ODBC makes no
+     * difference between SPARQL and SQL, because the result type is always an array.
+     * 
+     * @param string $queryString SPARQL- or SQL query to execute
+     * @param string $type optional Set type of statement: sparql (standard), 
+     *                              sparqlUpdate, sql. 
+     * @param string $graphUri optional URI of the graph to execute the (SPARQL) query in.
+     * @param array $variables optional Key-values pairs to fill placeholders in the query.
+     * @return \PDOStatement
+     * @throw \Exception If $queryString is invalid
+     */
+    public function executeQuery($queryString, $type = "sparql", $graphUri = null)
+    {
+        /**
+         * SPARQL query (usually to fetch data)
+         */
+        if ("sparql" == $type) {
+                        
+            if (true === \Saft\Uri::check($graphUri)) {
+                // enquote
+                $graphUri = "'" . $graphUri . "'";
+                $graphSpec = "define input:default-graph-uri <" . $graphUri . "> ";
+            } else {
+                // set Virtuoso NULL
+                $graphUri = "NULL";
+                $graphSpec = "";
+            }
+
+            // escape characters that delimit the query within the query
+            $queryString = $graphSpec . "CALL DB.DBA.SPARQL_EVAL('" . 
+                           addcslashes($queryString, '\'\\') . "', '" . 
+                           $graphUri . "', 0)";
+            
+        /**
+         * SPARPQL Update query
+         */
+        } elseif ("sparqlUpdate" == $type) {
+            $queryString = "SPARQL " . $queryString;
+        
+        /**
+         * SQL query
+         */
+        } else {
+            // nothing to do
+        }
+        
+        // execute query
+        try {
+            $query = $this->connect->prepare(
+                $queryString, array(\PDO::ATTR_CURSOR => \PDO::CURSOR_FWDONLY)
+            );
+            
+            $query->execute();
+            
+            return $query;
+            
+        } catch (\PDOException $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }    
+    
+    /**
+     * 
+     */
+    public function getAvailableGraphUris()
+    {
+        $query = $this->executeQuery(
+            "SELECT ID_TO_IRI(REC_GRAPH_IID) AS graph 
+               FROM DB.DBA.RDF_EXPLICITLY_CREATED_GRAPH", "sql"
+        );
+        
+        $graphs = array();
+        
+        foreach ($query->fetchAll(\PDO::FETCH_ASSOC) as $graph) {
+            $graphs[$graph["graph"]] = $graph["graph"];
+        }
+
+        return $graphs;
+    }
+    
+    /**
+     * Counts the number of triples in a graph.
+     *  
+     * @param string $graphUri URI of the graph you wanna count triples
+     * @return integer Number of found triples
+     * @throw \Exception
+     */
+    public function getTripleCount($graphUri)
+    {
+        $result = $this->sparql(
+            "SELECT COUNT(?s) as ?count FROM <$graphUri> WHERE {?s ?p ?o.}"
+        );
+        
+        return $result[0]["count"];
+    }
+    
+    /**
+     * Init adapter.
+     * 
+     * @param array $config Array containing database credentials
+     * @return
+     * @throw \Exception If requirements are not fullfilled. 
+     */
+    public function init(array $config)
+    {
+        $this->checkRequirements();
+        
+        $this->config = $config;
+        
+        // Open connection
+        $this->connect();
+    }
+    
+    /**
+     * Checks if a certain graph is available in the store.
+     *
+     * @param string $graphUri URI of the graph to check if it is available.
+     * @return boolean True if graph is available, false otherwise.
+     */
+    public function isGraphAvailable($graphUri)
+    {
+        $graphs = $this->getAvailableGraphUris();
+        
+        return true === isset($graphs[$graphUri]);
+    }
+    
+    /**
+     * Returns the current connection resource.
+     * The resource is created lazily if it doesn't exist.
+     * @retun resource
+     */
+    public function connect()
+    {
+        // connection still closed
+        if (!$this->connect) {
+            
+            $options = $this->config;
+            
+            // check for dsn parameter
+            if (!isset($options["dsn"])) {
+                throw new \Exception(
+                    "Parameter dsn is not set."
+                );
+            } else {
+                $dsn = (string) $options["dsn"];
+            }
+
+            // check for username parameter
+            if (!isset($options["username"])) {
+                throw new \Exception(
+                    "Parameter username is not set."
+                );
+            } else {
+                $username = (string) $options["username"];
+            }
+
+            // check for password parameter
+            if (!isset($options["password"])) {
+                throw new \Exception(
+                    "Parameter password is not set."
+                );
+            } else {
+                $password = (string) $options["password"];
+            }
+
+            /**
+             * Setup ODBC connection using PDO
+             */
+            try {
+                $this->connect = new \PDO("odbc:" . $dsn, $username, $password);
+                $this->connect->setAttribute(\PDO::ATTR_AUTOCOMMIT, false);
+                $this->connect->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+                $this->connect->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);                
+            
+            } catch (\PDOException $e) {
+                throw new \Exception($e->getMessage());
+            }
+            
+            $this->_user = $username;
+        }
+
+        return $this->connect;
+    }
+    
+    /**
+     * Send SPARQL query to the server.
+     * 
+     * @param string $query Query to execute
+     * @param array $variables optional Key-value-pairs to create prepared statements
+     * @param array $options optional Options to configure the query-execution and the result.
+     * @return array
+     * @throw \Exception
+     */
+    public function sparql($query, array $options = array())
+    {
+        /**
+         * result type not set, use array instead
+         */
+        if (false === isset($options["resultType"])) {
+            // if nothing was set, array is default result type
+            // possible are: array, extended
+            $options["resultType"] = "array";
+            
+        /**
+         * extended result type
+         */
+        } elseif ("array" != $options["resultType"] 
+                  && "extended" != $options["resultType"]) {
+            throw new \Exception(
+                "Given resultType is invalid, allowed are array and extended."
+            );
+        }
+        
+        // prepare query
+        $queryPrefix = "";
+        if ("extended" == $options["resultType"]) {
+            $queryPrefix = "define output:format \"JSON\"";
+        }
+        
+        $sparqlQuery = $queryPrefix . PHP_EOL . (string) $query;
+        $query = $this->executeQuery($sparqlQuery, "sparql");
+
+        if (false !== $query) {
+            
+            $result = $query->fetchAll(\PDO::FETCH_ASSOC);
+            
+            // encode as JSON string
+            if ("extended" === $options["resultType"]) {
+                $result = current(current($result));
+                $result = json_decode($result, true);
+            }
+        
+            return $result;
+        }
+    }
+}
