5fe6a1b0437fa718d8253bf4139ee79789119c8f
Konrad Abicht <k.abicht@googlemail.com>
2015-03-01 16:07:40 +0100

    Fix codesniffer issues

--- src/Saft/Cache/Adapter/PHPArray.php
+++ src/Saft/Cache/Adapter/PHPArray.php
@@ -8,20 +8,20 @@ class PHPArray extends \Saft\Cache\Adapter\AbstractAdapter
      * @var string
      */
     protected $cacheDir;
-    
+
     /**
-     * Checks that all requirements for this adapter are fullfilled. 
-     * 
+     * Checks that all requirements for this adapter are fullfilled.
+     *
      * @return boolean Returns true if all requirements are fullfilled.
      * @throws \Exception If one requirement is not fullfilled an exception will be thrown.
      */
     public function checkRequirements()
     {
         // no requirements to fullfill.
-        
+
         return true;
     }
-    
+
     /**
      * Clears cache array.
      */
@@ -29,19 +29,19 @@ class PHPArray extends \Saft\Cache\Adapter\AbstractAdapter
     {
         $this->cache = array();
     }
-    
+
     /**
      * Deletes entry by given $key.
-     * 
+     *
      * @param string $key
      */
     public function delete($key)
     {
         unset($this->cache[$key]);
     }
-    
+
     /**
-     * 
+     *
      * @param string $key
      * @return mixed|null
      * @throw
@@ -53,39 +53,39 @@ class PHPArray extends \Saft\Cache\Adapter\AbstractAdapter
         } else {
             return false;
         }
-    }  
-    
+    }
+
     /**
      * @return string Type of the cache.
      */
     public function getType()
     {
         return "phparray";
-    }     
-    
+    }
+
     /**
      * Check if key is cached.
-     * 
-     * @param string $key ID of the array entry.
+     *
+     * @param  string $key ID of the array entry.
      * @return boolean True, if entry behind given $key exists, false otherwise.
      */
     protected function isCached($key)
     {
         return true === isset($this->cache[$key]);
-    }     
-    
+    }
+
     /**
-     * @param string $key ID of the value to store.
-     * @param mixed $value Value to store.
+     * @param string $key   ID of the value to store.
+     * @param mixed  $value Value to store.
      */
     public function set($key, $value)
     {
         $this->cache[$key] = json_encode($value);
     }
-    
+
     /**
      * Setup cache adapter
-     * 
+     *
      * @param array $config Array containing necessary parameter to init instance.
      * @throw \Exception
      */
@@ -94,10 +94,11 @@ class PHPArray extends \Saft\Cache\Adapter\AbstractAdapter
         $this->cache = array();
         $this->config = $config;
     }
-    
+
     /**
-     * 
+     *
      */
     public function setup()
-    { }
+    {
+    }
 }
