125f129942cb047c42f8d83e81c60c822711d265
Konrad Abicht <k.abicht@googlemail.com>
2015-03-01 16:06:32 +0100

    Fix codesniffer issues

--- src/Saft/Cache.php
+++ src/Saft/Cache.php
@@ -10,8 +10,8 @@ class Cache
     /**
      * @var instance which implements \Enable\Cache\Adapter\Base
      */
-    protected $_cache;
-    
+    protected $cache;
+
     /**
      * @param array $config Array to configure the Cache instance.
      * @return void
@@ -20,116 +20,114 @@ class Cache
     {
         $this->init($config);
     }
-    
+
     /**
      * Removes all cached entries.
-     * 
+     *
      * @return void
      */
     public function clean()
     {
-        $this->_cache->clean();
+        $this->cache->clean();
     }
-    
+
     /**
      * Deletes a certain entry.
-     * 
-     * @param string $key ID of the entry to delete.
+     *
+     * @param  string $key ID of the entry to delete.
      * @return void
      */
     public function delete($key)
     {
-        $this->_cache->delete($key);
+        $this->cache->delete($key);
     }
-    
+
     /**
      * Returns the value of an entry, if it exists in the cache.
-     * 
-     * @param string $key ID of the entry.
+     *
+     * @param  string $key ID of the entry.
      * @return mixed Value of the entry.
      */
     public function get($key)
     {
-        return $this->_cache->get($key);
-    }  
-    
+        return $this->cache->get($key);
+    }
+
     /**
      * Returns the current active cache instance.
-     * 
+     *
      * @return mixed Instance which implements \Enable\Cache\Adapter\Base inteface
      */
     public function getCacheObj()
     {
-        return $this->_cache;
+        return $this->cache;
     }
-    
+
     /**
      * Returns the type of the cache adapter.
-     * 
+     *
      * @return string Type of the cache.
      */
     public function getType()
     {
         return $this->_config["type"];
-    }     
-    
+    }
+
     /**
      * Initialize the cache.
-     * 
+     *
      * @param array $config Array to configure this instance.
      * @throw \Exception In case of an unknown cache backend
      */
     public function init(array $config)
     {
         if (true === isset($config["type"])) {
-            
             switch ($config["type"]) {
-                
                 /**
                  * File
                  */
-                case "file": 
-                    $this->_cache = new \Saft\Cache\Adapter\File();
-                    $this->_cache->setup($config);
+                case "file":
+                    $this->cache = new \Saft\Cache\Adapter\File();
+                    $this->cache->setup($config);
                     break;
-                
+
                 /**
                  * MemcacheD
                  */
-                case "memcached": 
-                    $this->_cache = new \Saft\Cache\Adapter\MemcacheD();
-                    $this->_cache->setup($config);
+                case "memcached":
+                    $this->cache = new \Saft\Cache\Adapter\MemcacheD();
+                    $this->cache->setup($config);
                     break;
-                    
+
                 /**
                  * PHPArray
                  */
-                case "phparray": 
-                    $this->_cache = new \Saft\Cache\Adapter\PHPArray();
-                    $this->_cache->setup($config);
+                case "phparray":
+                    $this->cache = new \Saft\Cache\Adapter\PHPArray();
+                    $this->cache->setup($config);
                     break;
-                    
+
                 default:
                     throw new \Exception("Unknown cache backend.");
                     break;
             }
-        
+
         } else {
             throw new \Exception("Unknown cache backend.");
         }
-        
+
         $this->_config = $config;
     }
-    
+
     /**
      * Stores a new entry in the cache or overrides an existing one.
-     * 
-     * @param string $key Identifier of the value to store.
-     * @param mixed $value Value to store in the cache.
+     *
+     * @param  string $key   Identifier of the value to store.
+     * @param  mixed  $value Value to store in the cache.
      * @return void
      */
     public function set($key, $value)
     {
-        $this->_cache->set($key, $value);
+        $this->cache->set($key, $value);
     }
 }
