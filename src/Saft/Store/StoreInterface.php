95174d0c81ddcde8e49336c5a0333a514006602f
Viktor Befort <v.befort@googlemail.com>
2015-03-04 15:30:21 +0100

    delete redundant documentation

--- src/Saft/Store/StoreInterface.php
+++ src/Saft/Store/StoreInterface.php
@@ -24,9 +24,8 @@ interface StoreInterface
      * @param  StatementIterator $statements StatementList instance must contain Statement
      *                                  instances which are 'concret-' and not 'pattern'-
      *                                  statements.
-     * @param  string            $graphUri   optional Overrides target graph. If set, all statements will be add to that graph, if available. will be add to that graph, if available.
-     *                                  will be add to that graph, if available.
-     * @param  array             $options    optional It contains key-value pairs and should provide additional introductions for the store and/or its adapter(s). introductions for the store and/or its adapter(s).
+     * @param  string            $graphUri   optional Overrides target graph. If set, all statements will be add to that graph, if available.
+     * @param  array             $options    optional It contains key-value pairs and should provide additional
      *                                introductions for the store and/or its adapter(s).
      * @return boolean Returns true, if function performed without errors. In case
      *                 an error occur, an exception will be thrown.
@@ -41,9 +40,8 @@ interface StoreInterface
      * Removes all statements from a (default-) graph which match with given statement.
      *
      * @param  Statement $statement It can be either a concrete or pattern-statement.
-     * @param  string    $graphUri  optional Overrides target graph. If set, all statements will be delete in that graph. will be delete in that graph.
-     *                                  will be delete in that graph.
-     * @param  array     $options   optional It contains key-value pairs and should provide additional introductions for the store and/or its adapter(s). introductions for the store and/or its adapter(s).
+     * @param  string    $graphUri  optional Overrides target graph. If set, all statements will be delete in that graph.
+     * @param  array     $options   optional It contains key-value pairs and should provide additional
      *                                introductions for the store and/or its adapter(s).
      * @return boolean Returns true, if function performed without errors. In case
      *                 an error occur, an exception will be thrown.
@@ -61,9 +59,9 @@ interface StoreInterface
      * - statement's object is either equal to the object of a statement of the graph or it is null.
      *
      * @param  Statement $statement It can be either a concrete or pattern-statement.
-     * @param  string    $graphUri  optional Overrides target graph. If set, you will get all matching statements of that graph. matching statements of that graph.
+     * @param  string    $graphUri  optional Overrides target graph. If set, you will get all
      *                                  matching statements of that graph.
-     * @param  array     $options   optional It contains key-value pairs and should provide additional introductions for the store and/or its adapter(s). introductions for the store and/or its adapter(s).
+     * @param  array     $options   optional It contains key-value pairs and should provide additional
      *                                introductions for the store and/or its adapter(s).
      * @return StatementIterator It contains Statement instances
      *                           of all matching statements of the given graph.
@@ -80,7 +78,7 @@ interface StoreInterface
      *
      * @param  Statement $statement It can be either a concrete or pattern-statement.
      * @param  string    $graphUri  optional Overrides target graph.
-     * @param  array     $options   optional It contains key-value pairs and should provide additional introductions for the store and/or its adapter(s). introductions for the store and/or its adapter(s).
+     * @param  array     $options   optional It contains key-value pairs and should provide additional
      *                                introductions for the store and/or its adapter(s).
      * @return boolean Returns true if at least one match was found, false otherwise.
      */
@@ -101,7 +99,7 @@ interface StoreInterface
      * This method sends a SPARQL query to the store.
      *
      * @param  string $query   The SPARQL query to send to the store.
-     * @param  array  $options optional It contains key-value pairs and should provide additional introductions for the store and/or its adapter(s).
+     * @param  array  $options optional It contains key-value pairs and should provide additional
      *                                introductions for the store and/or its adapter(s).
      * @return Result Returns result of the query. Depending on the query
      *                type, it returns either an instance of ResultIterator, StatementIterator, or ResultValue
