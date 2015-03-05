763ad7005df78fcf81cabf443971dc2cc61d66fa
Konrad Abicht <k.abicht@googlemail.com>
2015-03-05 19:33:04 +0100

    Nice up code

--- test/unit/Saft/Store/AbstractSparqlStoreTest.php
+++ test/unit/Saft/Store/AbstractSparqlStoreTest.php
@@ -7,16 +7,19 @@ use Saft\Rdf\ArrayStatementIteratorImpl;
 
 class AbstractSparqlStoreTest extends TestCase
 {
+    /**
+     * Mock for AbstractSparqlStore
+     */
+    protected $fixture;
 
     public function setUp()
     {
         parent::setUp();
-        $this->store = $this->getMockForAbstractClass(
-            '\Saft\Store\AbstractSparqlStore'
-        );
-        //override query methode
-        $this->store->method('query')
-             ->will($this->returnArgument(0));
+        
+        $this->fixture = $this->getMockForAbstractClass('\Saft\Store\AbstractSparqlStore');
+        
+        // Override query method: it will always return the given query.
+        $this->fixture->method('query')->will($this->returnArgument(0));
     }
 
     public function testCreateStatement()
@@ -53,7 +56,7 @@ class AbstractSparqlStoreTest extends TestCase
      */
     public function testGetMatchingStatements(Statement $statement)
     {
-        $query = $this->store->getMatchingStatements($statement);
+        $query = $this->fixture->getMatchingStatements($statement);
         $this->assertEquals(
             $query,
             'SELECT * WHERE {Graph <> {<http://saft/test/s1> <http://saft/test/p1> <http://saft/test/o1>.} }'
@@ -65,12 +68,12 @@ class AbstractSparqlStoreTest extends TestCase
      */
     public function testAddStatements(ArrayStatementIteratorImpl $statements)
     {
-        $query = $this->store->addStatements($statements);
+        $query = $this->fixture->addStatements($statements);
 
         $this->assertEquals(
             $query,
-            "INSERT DATA {Graph <> {<http://saft/test/s1> <http://saft/test/p1> <http://saft/test/o1>.}"
-            . " Graph <http://saft/test/g2> {<http://saft/test/s2> <http://saft/test/p2> <http://saft/test/o2>.} }"
+            'INSERT DATA {Graph <> {<http://saft/test/s1> <http://saft/test/p1> <http://saft/test/o1>.}'.
+            ' Graph <http://saft/test/g2> {<http://saft/test/s2> <http://saft/test/p2> <http://saft/test/o2>.} }'
         );
     }
 
@@ -79,7 +82,7 @@ class AbstractSparqlStoreTest extends TestCase
      */
     public function testDeleteMatchingStatements(Statement $statement)
     {
-        $query = $this->store->deleteMatchingStatements($statement);
+        $query = $this->fixture->deleteMatchingStatements($statement);
         //echo $query;
         $this->assertEquals(
             $query,
@@ -92,7 +95,7 @@ class AbstractSparqlStoreTest extends TestCase
      */
     public function testhasMatchingStatement(Statement $statement)
     {
-        $query = $this->store->hasMatchingStatement($statement);
+        $query = $this->fixture->hasMatchingStatement($statement);
         //echo $query;
         $this->assertEquals(
             $query,
@@ -102,42 +105,47 @@ class AbstractSparqlStoreTest extends TestCase
 
     public function testMultipleVariatonOfStatements()
     {
-        //object is a number
+        /**
+         * object is a number
+         */
         $subject1 = new \Saft\Rdf\NamedNode('http://saft/test/s1');
         $predicate1 = new \Saft\Rdf\NamedNode('http://saft/test/p1');
         $object1 = new \Saft\Rdf\Literal(42);
         $graph1 = new \Saft\Rdf\NamedNode(null);
         $triple1 = new \Saft\Rdf\StatementImpl($subject1, $predicate1, $object1, $graph1);
 
-        //object is a literal
+        /**
+         * object is a literal
+         */
         $object2 = new \Saft\Rdf\Literal('"John"');
         $triple2 = new \Saft\Rdf\StatementImpl($subject1, $predicate1, $object2, $graph1);
-        $statements = new \Saft\Rdf\ArrayStatementIteratorImpl(
-            array($triple1, $triple2)
-        );
-
-        $query = $this->store->addStatements($statements);
-        $this->assertEquals(
-            $query,
-            'INSERT DATA {Graph <> {<http://saft/test/s1> <http://saft/test/p1> "42"^^<http://www.w3.org/2001/XMLSchema#integer>.}'
-            .' Graph <> {<http://saft/test/s1> <http://saft/test/p1> ""John""^^<http://www.w3.org/2001/XMLSchema#string>.} }'
-        );
-
         
-        //use the given graphUri
-        $statements = new \Saft\Rdf\ArrayStatementIteratorImpl(
-            array($triple1)
-        );
-        $query = $this->store->addStatements($statements, "http://saft/test/graph");
+        // Setup array statement iterator
+        $statements = new \Saft\Rdf\ArrayStatementIteratorImpl(array($triple1, $triple2));
+
+        // add test statements
+        $query = $this->fixture->addStatements($statements);
         $this->assertEquals(
             $query,
-            'INSERT DATA {Graph <http://saft/test/graph> {<http://saft/test/s1> <http://saft/test/p1> "42"^^<http://www.w3.org/2001/XMLSchema#integer>.} }'
+            'INSERT DATA {Graph <> {'.
+            '<http://saft/test/s1> <http://saft/test/p1> "42"^^<http://www.w3.org/2001/XMLSchema#integer>.'.
+            '} Graph <> {'.
+            '<http://saft/test/s1> <http://saft/test/p1> ""John""^^<http://www.w3.org/2001/XMLSchema#string>.'.
+            '} }'
         );
-    }
 
-    public function tearDown()
-    {
-        unset($this->store);
-        parent::tearDown();
+        /**
+         * use the given graphUri
+         */
+        $statements = new \Saft\Rdf\ArrayStatementIteratorImpl(array($triple1));
+        
+        $query = $this->fixture->addStatements($statements, 'http://saft/test/graph');
+        
+        $this->assertEquals(
+            $query,
+            'INSERT DATA {Graph <http://saft/test/graph> {'.
+            '<http://saft/test/s1> <http://saft/test/p1> "42"^^<http://www.w3.org/2001/XMLSchema#integer>.'.
+            '} }'
+        );
     }
 }
