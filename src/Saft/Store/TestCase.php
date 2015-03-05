2c65edc4ff7286f4e049a96b46d8b9f50b4142d9
Konrad Abicht <k.abicht@googlemail.com>
2015-03-05 19:21:01 +0100

    Remove obsolete Graph.php and Store.php (+ tests); setup leightweight phpunit

--- src/Saft/Store/TestCase.php
+++ src/Saft/Store/TestCase.php
@@ -42,26 +42,4 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($expected, $actual, $message);
     }
-
-    /**
-     *
-     */
-    public function setUp()
-    {
-        parent::setUp();
-
-        // set path to root dir, usually where to saft-skeleton
-        // TODO move config.yml stuff to Saft.store package
-        $saftRootDir = dirname(__FILE__) . '/../../../';
-        $configFilepath = $saftRootDir . 'test/config.yml';
-
-        // check for config file
-        if (false === file_exists($configFilepath)) {
-            throw new \Exception('config.yml missing in test/config.yml');
-        }
-
-        // parse YAML file
-        $yaml = new Parser();
-        $this->config = $yaml->parse(file_get_contents($configFilepath));
-    }
 }
