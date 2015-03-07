e201f1fc5e12603e24aa19dbba50b606e29fbb36
Natanael Arndt <arndtn@gmail.com>
2015-03-07 16:35:30 +0100

    Add reference to php streams

--- src/Saft/Data/ParserInterface.php
+++ src/Saft/Data/ParserInterface.php
@@ -7,7 +7,9 @@ interface ParserInterface
      * @note We have to decide how the ParserInterface should be implemented. One option
      * could be that a parser can accept multiple files/streams which are handled as one
      * graph and all statements are combined in the resulting StatementIterator.
-     * So eather addFile or setFile should be used
+     * So eather addFile or setFile should be used.
+     * Maybe the PHP Stream API will be relevant here:
+     * http://php.net/manual/en/book.stream.php
      * @unstable
      */
     public function addFile();
