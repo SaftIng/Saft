a31bd13101912febad46a66bafb0c7ba5f99c9c7
Konrad Abicht <k.abicht@googlemail.com>
2015-03-11 18:52:01 +0100

    Port to new CacheInterface; change " to '; adapt function comments

--- src/Saft/Cache/Adapter/PHPArray.php
+++ src/Saft/Cache/Adapter/PHPArray.php
@@ -2,18 +2,15 @@
 
 namespace Saft\Cache\Adapter;
 
-class PHPArray extends \Saft\Cache\Adapter\AbstractAdapter
-{
-    /**
-     * @var string
-     */
-    protected $cacheDir;
+use Saft\Cache\CacheInterface;
 
+class PHPArray implements CacheInterface
+{
     /**
      * Checks that all requirements for this adapter are fullfilled.
      *
-     * @return boolean Returns true if all requirements are fullfilled.
-     * @throws \Exception If one requirement is not fullfilled an exception will be thrown.
+     * @return boolean Returns true if all requirements are fullfilled, false otherwise
+     * @throws \Exception If one requirement is not fullfilled.
      */
     public function checkRequirements()
     {
@@ -23,7 +20,7 @@ class PHPArray extends \Saft\Cache\Adapter\AbstractAdapter
     }
 
     /**
-     * Clears cache array.
+     * Removes all entries of the cache instance.
      */
     public function clean()
     {
@@ -31,9 +28,9 @@ class PHPArray extends \Saft\Cache\Adapter\AbstractAdapter
     }
 
     /**
-     * Deletes entry by given $key.
+     * Deletes a certain cache entry by key.
      *
-     * @param string $key
+     * @param string $key Key of the cache entry to delete.
      */
     public function delete($key)
     {
@@ -41,10 +38,10 @@ class PHPArray extends \Saft\Cache\Adapter\AbstractAdapter
     }
 
     /**
+     * Returns the value to a given key, if it exists in the cache.
      *
-     * @param string $key
-     * @return mixed|null
-     * @throw
+     * @param string $key ID of the entry to return the value from.
+     * @return mixed Value of the entry.
      */
     public function get($key)
     {
@@ -56,27 +53,31 @@ class PHPArray extends \Saft\Cache\Adapter\AbstractAdapter
     }
 
     /**
-     * @return string Type of the cache.
+     * Returns the type of the cache adapter.
+     *
+     * @return string Type of the cache adapter.
      */
     public function getType()
     {
-        return "phparray";
+        return 'phparray';
     }
 
     /**
-     * Check if key is cached.
-     *
-     * @param  string $key ID of the array entry.
+     * Checks if an entry is cached.
+     * 
+     * @param string $key ID of the entry to check.
      * @return boolean True, if entry behind given $key exists, false otherwise.
      */
-    protected function isCached($key)
+    public function isCached($key)
     {
         return true === isset($this->cache[$key]);
     }
 
     /**
-     * @param string $key   ID of the value to store.
-     * @param mixed  $value Value to store.
+     * Stores a new entry in the cache or overrides an existing one.
+     *
+     * @param string $key Identifier of the value to store.
+     * @param mixed $value Value to store in the cache.
      */
     public function set($key, $value)
     {
@@ -84,21 +85,15 @@ class PHPArray extends \Saft\Cache\Adapter\AbstractAdapter
     }
 
     /**
-     * Setup cache adapter
+     * Setup cache adapter. All operations to establish a connection to the cache have to be done. It should 
+     * call checkRequirements to be sure all requirements are fullfilled, before init anything.
      *
-     * @param array $config Array containing necessary parameter to init instance.
-     * @throw \Exception
+     * @param array $config Array containing necessary parameter to setup a cache adapter.
+     * @throws \Exception If one requirement is not fullfilled.
      */
-    public function init(array $config)
+    public function setup(array $config)
     {
         $this->cache = array();
         $this->config = $config;
-    }
-
-    /**
-     *
-     */
-    public function setup()
-    {
     }
 }
