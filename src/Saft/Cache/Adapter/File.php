fbaed7495cec5787cd54eae679a433edeed98345
Konrad Abicht <k.abicht@googlemail.com>
2015-03-11 18:52:48 +0100

    Port to Saft\Cache\CacheInterface; Change " to '; adapt function comments

--- src/Saft/Cache/Adapter/File.php
+++ src/Saft/Cache/Adapter/File.php
@@ -2,7 +2,9 @@
 
 namespace Saft\Cache\Adapter;
 
-class File extends \Saft\Cache\Adapter\AbstractAdapter
+use Saft\Cache\CacheInterface;
+
+class File implements CacheInterface
 {
     /**
      * @var string
@@ -13,12 +15,26 @@ class File extends \Saft\Cache\Adapter\AbstractAdapter
      * @var string
      */
     protected $tempDir;
+    
+    /**
+     * Checks that all requirements for this adapter are fullfilled.
+     *
+     * @return boolean Returns true if all requirements are fullfilled, false otherwise
+     * @throws \Exception If one requirement is not fullfilled.
+     */
+    public function checkRequirements()
+    {
+        if (true === is_readable($this->tempDir) && true === is_writable($this->tempDir)) {
+            return true;
+        } else {
+            throw new \Enable\Exception(
+                'Systems temporary folder is either not readable or writable.'
+            );
+        }
+    }
 
     /**
-     *
-     * @param
-     * @return
-     * @throw
+     * Removes all entries of the cache instance.
      */
     public function clean()
     {
@@ -31,33 +47,32 @@ class File extends \Saft\Cache\Adapter\AbstractAdapter
     }
 
     /**
+     * Deletes a certain cache entry by key.
      *
-     * @param
-     * @return
-     * @throw
+     * @param string $key Key of the cache entry to delete.
      */
     public function delete($key)
     {
-        $filename = hash("sha256", $key);
+        $filename = hash('sha256', $key);
 
         if (true === $this->isCached($key)) {
-            unlink($this->cacheDir . $filename .".cache");
+            unlink($this->cacheDir . $filename .'.cache');
         }
     }
 
     /**
+     * Returns the value to a given key, if it exists in the cache.
      *
-     * @param
-     * @return
-     * @throw
+     * @param string $key ID of the entry to return the value from.
+     * @return mixed Value of the entry.
      */
     public function get($key)
     {
-        $filename = hash("sha256", $key);
+        $filename = hash('sha256', $key);
 
         if (true === $this->isCached($key)) {
             return json_decode(
-                file_get_contents($this->cacheDir . $filename . ".cache"),
+                file_get_contents($this->cacheDir . $filename . '.cache'),
                 true
             );
         } else {
@@ -66,54 +81,57 @@ class File extends \Saft\Cache\Adapter\AbstractAdapter
     }
 
     /**
-     * @return string Type of the cache.
+     * Returns the type of the cache adapter.
+     *
+     * @return string Type of the cache adapter.
      */
     public function getType()
     {
-        return $this->_config["type"];
+        return $this->_config['type'];
     }
 
     /**
-     * @param string $key ID of the file to check.
-     * @return boolean True, if file behind given $key exists, false otherwise.
+     * Checks if an entry is cached.
+     * 
+     * @param string $key ID of the entry to check.
+     * @return boolean True, if entry behind given $key exists, false otherwise.
      */
-    protected function isCached($key)
+    public function isCached($key)
     {
-        $filename = hash("sha256", $key);
+        $filename = hash('sha256', $key);
 
-        return true === file_exists($this->cacheDir . $filename .".cache");
+        return true === file_exists($this->cacheDir . $filename .'.cache');
     }
 
     /**
-     * @param string $key   ID of the value to store.
-     * @param mixed  $value Value to store.
-     * @return
-     * @throw
+     * Stores a new entry in the cache or overrides an existing one.
+     *
+     * @param string $key Identifier of the value to store.
+     * @param mixed $value Value to store in the cache.
      */
     public function set($key, $value)
     {
-        $filename = hash("sha256", $key);
+        $filename = hash('sha256', $key);
         $value = json_encode($value);
 
-        file_put_contents($this->cacheDir . $filename .".cache", $value);
+        file_put_contents($this->cacheDir . $filename .'.cache', $value);
     }
 
     /**
-     * Setup File cache adapter
+     * Setup cache adapter. All operations to establish a connection to the cache have to be done. It should 
+     * call checkRequirements to be sure all requirements are fullfilled, before init anything.
      *
-     * @param array $config Array containing necessary parameter to setup the
-     *                      server.
-     * @throw \Enable\Exception
+     * @param array $config Array containing necessary parameter to setup a cache adapter.
+     * @throws \Exception If one requirement is not fullfilled.
+     * @todo support the use of user defined dir
      */
     public function setup(array $config)
     {
         // save reference to systems temp directory
         $this->tempDir = sys_get_temp_dir();
 
-        if (true === is_readable($this->tempDir)
-            && true === is_writable($this->tempDir)
-        ) {
-            $this->cacheDir = $this->tempDir . "/enable/";
+        if (true === $this->checkRequirements()) {
+            $this->cacheDir = $this->tempDir . '/saft/';
 
             try {
                 // if caching folder does not exists, create it
@@ -121,15 +139,10 @@ class File extends \Saft\Cache\Adapter\AbstractAdapter
                     mkdir($this->cacheDir, 0744);
                 }
             } catch (\Exception $e) {
-                throw new \Enable\Exception($e->getMessage());
+                throw new \Exception($e->getMessage());
             }
 
             $this->_config = $config;
-
-        } else {
-            throw new \Enable\Exception(
-                "Systems temporary folder is either not readable or writable."
-            );
         }
     }
 }
