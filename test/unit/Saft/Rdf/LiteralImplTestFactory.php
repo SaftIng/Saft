d5aff34b0e1ed414db32bbdc99da513cbed11288
Natanael Arndt <arndtn@gmail.com>
2015-03-21 18:25:53 +0100

    Also introduce factories for test framework

--- test/unit/Saft/Rdf/LiteralImplTestFactory.php
+++ test/unit/Saft/Rdf/LiteralImplTestFactory.php
@@ -1,16 +1,10 @@
 <?php
 namespace Saft\Rdf;
 
-class LiteralImplTest extends Test\LiteralAbstractTest
+class LiteralImplTestFactory
 {
-
-    /**
-     * Return a new instance of LiteralImpl
-     */
     public function newInstance($value, $param = null)
     {
         return new LiteralImpl($value, $param);
     }
-
-
 }
