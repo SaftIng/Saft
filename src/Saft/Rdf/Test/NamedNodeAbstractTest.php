3cc30996b4c64ccf8bd1d977ac9eb9bb2674dcb0
Natanael Arndt <arndtn@gmail.com>
2015-03-28 10:09:33 +0100

    Make named node test abstract

---
+++ src/Saft/Rdf/Test/NamedNodeAbstractTest.php
@@ -0,0 +1,100 @@
+<?php
+namespace Saft\Rdf\Test;
+
+
+abstract class NamedNodeAbstractTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * An abstract method which returns new instances of Literal
+     * @todo somehow also support datatypes
+     * @todo The factory method approach could also be extended to use a factory object
+     * @param Literal
+     */
+    abstract public function newInstance($uri);
+
+
+    /**
+     * Tests check
+     * What is this check for? There shouldn't be a method check
+     */
+    public function testCheck()
+    {
+        $fixture = $this->newInstance('http://saft/test');
+        $this->assertFalse($fixture->check(''));
+        $this->assertFalse($fixture->check('http//foobar/'));
+
+        $this->assertTrue($fixture->check('http:foobar/'));
+        $this->assertTrue($fixture->check('http://foobar/'));
+        $this->assertTrue($fixture->check('http://foobar:42/'));
+        $this->assertTrue($fixture->check('http://foo:bar@foobar/'));
+    }
+
+    /**
+     * Tests instanciation
+     */
+    public function testInstanciationInvalidUri()
+    {
+        $this->setExpectedException('\Exception');
+
+        $this->newInstance('foo');
+    }
+
+    public function testInstanciationNull()
+    {
+        $this->setExpectedException('\Exception');
+
+        $this->newInstance(null);
+    }
+
+    public function testInstanciationValidUri()
+    {
+        $this->newInstance('http://saft/test');
+        $this->assertEquals('http://saft/test', $this->fixture->getValue());
+    }
+
+    /**
+     * Tests isBlank
+     */
+    public function testIsBlank()
+    {
+        $fixture = $this->newInstance('http://saft/test');
+        $this->assertFalse($fixture->isBlank());
+    }
+
+    /**
+     * Tests isConcrete
+     */
+    public function testIsConcrete()
+    {
+        $fixture = $this->newInstance('http://saft/test');
+        $this->assertTrue($fixture->isConcrete());
+    }
+
+    /**
+     * Tests isLiteral
+     */
+    public function testIsLiteral()
+    {
+        $fixture = $this->newInstance('http://saft/test');
+        $this->assertFalse($fixture->isLiteral());
+    }
+
+    /**
+     * Tests isNamed
+     */
+    public function testIsNamed()
+    {
+        $fixture = $this->newInstance('http://saft/test');
+        $this->assertTrue($fixture->isNamed());
+    }
+
+    /**
+     * Tests isVariable
+     */
+    public function testIsVariable()
+    {
+        $fixture = $this->newInstance('http://saft/test');
+        $this->assertFalse($fixture->isVariable());
+    }
+}
--- src/Saft/Rdf/Test/NamedNodeImplUnitTest.php
+++ src/Saft/Rdf/Test/NamedNodeImplUnitTest.php
@@ -7,100 +7,11 @@ use Saft\Rdf\NamedNodeImpl;
 class NamedNodeImplUnitTest extends \PHPUnit_Framework_TestCase
 {
 
-    protected $testUri = 'http://saft/test/';
-
     /**
      *
      */
-    public function setUp()
+    public function newInstance($uri)
     {
-        parent::setUp();
-
-        $this->fixture = new NamedNodeImpl($this->testUri);
-    }
-
-    /**
-     *
-     */
-    public function tearDown()
-    {
-        parent::tearDown();
-    }
-
-    /**
-     * Tests check
-     */
-    public function testCheck()
-    {
-        $this->assertFalse($this->fixture->check(''));
-        $this->assertFalse($this->fixture->check('http//foobar/'));
-
-        $this->assertTrue($this->fixture->check('http:foobar/'));
-        $this->assertTrue($this->fixture->check('http://foobar/'));
-        $this->assertTrue($this->fixture->check('http://foobar:42/'));
-        $this->assertTrue($this->fixture->check('http://foo:bar@foobar/'));
-    }
-
-    /**
-     * Tests instanciation
-     */
-    public function testInstanciationInvalidUri()
-    {
-        $this->setExpectedException('\Exception');
-
-        $this->fixture = new NamedNodeImpl('foo');
-    }
-
-    public function testInstanciationNull()
-    {
-        $this->fixture = new NamedNodeImpl(null);
-        $this->assertEquals(null, $this->fixture->getValue());
-    }
-
-    public function testInstanciationValidUri()
-    {
-        $this->fixture = new NamedNodeImpl($this->testUri);
-        $this->assertEquals($this->testUri, $this->fixture->getValue());
-    }
-
-    /**
-     * Tests isBlank
-     */
-    public function testIsBlank()
-    {
-        $this->assertFalse($this->fixture->isBlank());
-    }
-
-    /**
-     * Tests isConcrete
-     */
-    public function testIsConcrete()
-    {
-        $this->fixture = new NamedNodeImpl($this->testUri);
-        $this->assertTrue($this->fixture->isConcrete());
-    }
-
-    /**
-     * Tests isLiteral
-     */
-    public function testIsLiteral()
-    {
-        $this->assertFalse($this->fixture->isLiteral());
-    }
-
-    /**
-     * Tests isNamed
-     */
-    public function testIsNamed()
-    {
-        $this->assertTrue($this->fixture->isNamed());
-    }
-
-    /**
-     * Tests isVariable
-     */
-    public function testIsVariable()
-    {
-        $this->assertFalse($this->fixture->isVariable());
+        return new NamedNodeImpl($uri);
     }
 }
