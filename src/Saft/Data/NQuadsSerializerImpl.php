12c252e9e869aad3567447c4756e3784ab13b1ce
Natanael Arndt <arndtn@gmail.com>
2015-03-29 18:23:41 +0200

    Update serializer interface and add basic implementation

---
+++ src/Saft/Data/NQuadsSerializerImpl.php
@@ -0,0 +1,42 @@
+<?php
+namespace Saft\Data;
+
+use Saft\Rdf\StatementIterator;
+
+class NQuadsSerializerImpl implements SerializerInterface {
+
+    /**
+     * Prefixes are ignored in the NQuads serialization
+     * @param $prefixes array will be ignored
+     * @return void
+     */
+    public function setPrefixes(array $prefixes)
+    {}
+
+    /**
+     * @unstable
+     * @param $outputStream string filename of the stream to where the serialization should be written
+     *                             {@url http://php.net/manual/en/book.stream.php}
+     * @param $statements StatementIterator The StatementIterator containing all the Statements which should be
+     *                                      serialized by the serializer.
+     * @param $serialization string the serialization which should be used. If null N-Quads serialization will be used.
+     * @return void
+     */
+    public function serializeIteratorToStream($outputStream, StatementIterator $statements, $serialization = null)
+    {
+        $handler = fopen($outputStream, 'a');
+        foreach ($statements as $statement) {
+            fwrite($handler, $statement->toNQuads() . PHP_EOL);
+        }
+        fclose($handler);
+    }
+
+    /**
+     * @unstable
+     * @return array of supported mime types which can be used by this serializer
+     */
+    public function getSupportedSerializations()
+    {
+        return ["application/n-triples", "application/n-quads"];
+    }
+}
--- src/Saft/Data/SerializerInterface.php
+++ src/Saft/Data/SerializerInterface.php
@@ -1,6 +1,35 @@
 <?php
 namespace Saft\Data;
 
+use Saft\Rdf\StatementIterator;
+
 interface SerializerInterface
 {
+    /**
+     * Set the prefixes which the serializer can/should use when generating the serialization. Please keep in mind, that
+     * some serializations don't support prefixes at all or that some implementations might ignore them.
+     * @param $prefixes array An associative array with a prefix mapping of the prefixes. The key will be the prefix,
+     *                        while the values contains the according namespace URI.
+     * @return void
+     */
+    public function setPrefixes(array $prefixes);
+
+    /**
+     * @unstable
+     * @param $outputStream string filename of the stream to where the serialization should be written
+     *                             {@url http://php.net/manual/en/book.stream.php}
+     * @param $statements StatementIterator The StatementIterator containing all the Statements which should be
+     *                                      serialized by the serializer.
+     * @param $serialization string the serialization which should be used. If null is given the serializer will either
+     *                              apply some default serialization, or the only one it is supporting, or will throw an
+     *                              Exception.
+     * @return void
+     */
+    public function serializeIteratorToStream($outputStream, StatementIterator $statements, $serialization = null);
+
+    /**
+     * @unstable
+     * @return array of supported mime types which can be used by this serializer
+     */
+    public function getSupportedSerializations();
 }
