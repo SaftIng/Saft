6aae9c13f2ed99fd3dae9ac67ffbe7134225a4c4
Oliver Skawronek <oskawron@imn.htwk-leipzig.de>
2015-04-03 18:21:47 +0200

    Add CommentLineIgnoringIterator as a basement for a statement iterator on ntriples files

---
+++ src/Saft/Backend/LocalStore/Store/CommentIgnoringLineIterator.php
@@ -0,0 +1,150 @@
+<?php
+namespace Saft\Backend\LocalStore\Store;
+
+/**
+ * Iterates over a file line by line, ignoring comment lines.
+ * A comment line has a hash '#' as its first character.
+ * Because the text file is opened, after using the iterator,
+ * you have to close it.
+ */
+final class CommentIgnoringLineIterator implements \Iterator
+{
+    private $pointer;
+    private $closed = false;
+    // null or a non-comment line string
+    private $currentLine = null;
+    // starting by 0
+    private $lineCount = 0;
+
+    /**
+     * @param string $filename
+     * @throws \Exception When file can't opened
+     */
+    public function __construct($filename)
+    {
+        if (is_null($filename)) {
+            throw new \InvalidArgumentException('');
+        }
+
+        $this->pointer = fopen($filename, 'r');
+        if ($this->pointer === false) {
+            throw new \Exception('Unable to open ' . $filename);
+        }
+    }
+
+    /**
+     * You must call rewind at first.
+     * @return string Current non-comment line
+     */
+    public function current()
+    {
+        $this->ensureNotClosed();
+        if (!$this->valid()) {
+            throw new \Exception('No such element');
+        }
+        return $this->currentLine;
+    }
+
+    /**
+     * @return int Line number of the current line starting at 0
+     */
+    public function key()
+    {
+        $this->ensureNotClosed();
+        if (!$this->valid()) {
+            throw new \Exception('No such element');
+        }
+        return $this->lineCount;
+    }
+
+    /**
+     * Moves to the next non-comment line.
+     */
+    public function next()
+    {
+        $this->ensureNotClosed();
+        if (!$this->valid()) {
+            throw new \Exception('No such element');
+        }
+        $this->currentLine = null;
+        $this->skipComments();
+    }
+
+    /**
+     * Reset to the first line.
+     */
+    public function rewind()
+    {
+        $this->ensureNotClosed();
+        if (rewind($this->pointer) === false) {
+            throw new \Exception('Unable to rewind');
+        }
+        $this->currentLine = null;
+        $this->lineCount = 0;
+        $this->skipComments();
+    }
+
+    /**
+     * @return boolean true, if next can called, else false.
+     */
+    public function valid()
+    {
+        return !is_null($this->currentLine);
+    }
+    
+    private function skipComments()
+    {
+        while (!feof($this->pointer)) {
+            $line = $this->readLine();
+            if (is_null($line)) {
+                // feof is not true at start but fgets returns false
+                // when dealing with empty files.
+                break;
+            } elseif (!$this->isComment($line)) {
+                $this->currentLine = rtrim($line, "\n\r");
+                break;
+            }
+        }
+    }
+
+    private function readLine()
+    {
+        $line = fgets($this->pointer);
+        if ($line !== false) {
+            $this->lineCount++;
+            return $line;
+        } else {
+            return null;
+        }
+    }
+
+    private function isComment($line)
+    {
+        return (strpos($line, '#') === 0);
+    }
+
+    /**
+     * Close the iterator. The file gets closed. If allready closed,
+     * this has no effect.
+     */
+    public function close()
+    {
+        if (!is_null($this->pointer)) {
+            fclose($this->pointer);
+            $this->pointer = null;
+            $this->closed = true;
+        }
+    }
+
+    public function isClosed()
+    {
+        return $this->closed;
+    }
+
+    private function ensureNotClosed()
+    {
+        if ($this->closed) {
+            throw new \Exception('Closed');
+        }
+    }
+}
---
+++ src/Saft/Backend/LocalStore/Test/CommentIgnoringLineIteratorUnitTest.php
@@ -0,0 +1,178 @@
+<?php
+namespace Saft\Backend\LocalStore\Test;
+
+use Saft\Backend\LocalStore\Store\CommentIgnoringLineIterator;
+
+class CommentIgnoringLineIteratorUnitTest extends \PHPUnit_Framework_TestCase
+{
+    // Used for temporary text files
+    private $tempDirectory = null;
+
+    public function setUp()
+    {
+        $this->tempDirectory = TestUtil::createTempDirectory();
+    }
+
+    public function tearDown()
+    {
+        if (!is_null($this->tempDirectory)) {
+            TestUtil::deleteDirectory($this->tempDirectory);
+        }
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testConstructorChecksForNull()
+    {
+        new CommentIgnoringLineIterator(null);
+    }
+
+    const NUM_REWINDS = 3;
+
+    const NO_CONTENT = '';
+
+    public function testNoContent()
+    {
+        $filename = $this->randomFileName();
+        $this->writeFile($filename, self::NO_CONTENT);
+
+        $it = new CommentIgnoringLineIterator($filename);
+        for ($i = 1; $i <= self::NUM_REWINDS; $i++) {
+            $it->rewind();
+            $this->assertFalse($it->valid());
+        }
+        $it->close();
+    }
+
+    const ONLY_ONE_COMMENT = <<<EOD
+#Some comment
+EOD;
+
+    public function testOnlyOneComment()
+    {
+        $filename = $this->randomFileName();
+        $this->writeFile($filename, self::ONLY_ONE_COMMENT);
+
+        $it = new CommentIgnoringLineIterator($filename);
+        for ($i = 1; $i <= self::NUM_REWINDS; $i++) {
+            $it->rewind();
+            $this->assertFalse($it->valid());
+        }
+        $it->close();
+    }
+
+    const ONLY_CONTENT = <<<EOD
+Lorem ipsum dolor sit amet,
+consetetur sadipscing elitr,
+EOD;
+
+    public function testOnlyContent()
+    {
+        $filename = $this->randomFileName();
+        $this->writeFile($filename, self::ONLY_CONTENT);
+
+        $it = new CommentIgnoringLineIterator($filename);
+        for ($i = 1; $i <= self::NUM_REWINDS; $i++) {
+            $it->rewind();
+            $this->assertTrue($it->valid());
+            $this->assertEquals('Lorem ipsum dolor sit amet,', $it->current());
+            $it->next();
+            $this->assertEquals('consetetur sadipscing elitr,', $it->current());
+            $it->next();
+            $this->assertFalse($it->valid());
+        }
+        $it->close();
+    }
+
+    const STARTING_COMMENT = <<<EOD
+#Prolog
+Lorem ipsum dolor sit amet,
+#Some comment
+consetetur sadipscing elitr,
+EOD;
+
+    public function testStartingComment()
+    {
+        $filename = $this->randomFileName();
+        $this->writeFile($filename, self::STARTING_COMMENT);
+
+        $it = new CommentIgnoringLineIterator($filename);
+        for ($i = 1; $i <= self::NUM_REWINDS; $i++) {
+            $it->rewind();
+            $this->assertTrue($it->valid());
+            $this->assertEquals('Lorem ipsum dolor sit amet,', $it->current());
+            $it->next();
+            $this->assertEquals('consetetur sadipscing elitr,', $it->current());
+            $it->next();
+            $this->assertFalse($it->valid());
+        }
+        $it->close();
+    }
+
+    const ENDING_COMMENT = <<<EOD
+Lorem ipsum dolor sit amet,
+#Some comment
+consetetur sadipscing elitr,
+#Epilog
+EOD;
+
+    public function testEndingComment()
+    {
+        $filename = $this->randomFileName();
+        $this->writeFile($filename, self::ENDING_COMMENT);
+
+        $it = new CommentIgnoringLineIterator($filename);
+        for ($i = 1; $i <= self::NUM_REWINDS; $i++) {
+            $it->rewind();
+            $this->assertTrue($it->valid());
+            $this->assertEquals('Lorem ipsum dolor sit amet,', $it->current());
+            $it->next();
+            $this->assertEquals('consetetur sadipscing elitr,', $it->current());
+            $it->next();
+            $this->assertFalse($it->valid());
+        }
+        $it->close();
+    }
+
+    const STARTING_AND_ENDING_COMMENT = <<<EOD
+#Prolog
+Lorem ipsum dolor sit amet,
+#Some comment
+consetetur sadipscing elitr,
+#Epilog
+EOD;
+
+    public function testStartingAndEndingComment()
+    {
+        $filename = $this->randomFileName();
+        $this->writeFile($filename, self::STARTING_AND_ENDING_COMMENT);
+
+        $it = new CommentIgnoringLineIterator($filename);
+        for ($i = 1; $i <= self::NUM_REWINDS; $i++) {
+            $it->rewind();
+            $this->assertTrue($it->valid());
+            $this->assertEquals('Lorem ipsum dolor sit amet,', $it->current());
+            $it->next();
+            $this->assertEquals('consetetur sadipscing elitr,', $it->current());
+            $it->next();
+            $this->assertFalse($it->valid());
+        }
+        $it->close();
+    }
+
+    private function randomFileName()
+    {
+        return $this->tempDirectory . DIRECTORY_SEPARATOR . uniqid() . '.txt';
+    }
+
+    private static function writeFile($filename, $content)
+    {
+        $file = fopen($filename, 'w');
+        if ($file === false) {
+            throw new \Exception('Unable to write file ' . $filename);
+        }
+        fwrite($file, $content);
+        fclose($file);
+    }
+}
