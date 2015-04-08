6f8a9878c2befb2327b4d9ab6a97e73ccce5d296
Viktor Befort <v.befort@googlemail.com>
2015-04-08 20:50:48 +0200

    old test restored

--- src/Saft/Store/Test/AbstractTriplePatternStoreUnitTest.php
+++ src/Saft/Store/Test/AbstractTriplePatternStoreUnitTest.php
@@ -10,14 +10,7 @@ use Saft\Rdf\VariableImpl;
 use Saft\Rdf\Statement;
 use Saft\Rdf\StatementImpl;
 
-/**
- * Because this class has no tests, the extends part was commented out, otherwise PHPUnit throws a warning:
- * 1) Warning
- * No tests found in class "Saft\Store\Test\AbstractTriplePatternStoreUnitTest".
- * phar:///usr/bin/phpunit/phpunit/TextUI/Command.php:176
- * phar:///usr/bin/phpunit/phpunit/TextUI/Command.php:129
- */
-class AbstractTriplePatternStoreUnitTest // extends TestCase
+class AbstractTriplePatternStoreUnitTest extends TestCase
 {
     protected function setUp()
     {
@@ -46,64 +39,52 @@ class AbstractTriplePatternStoreUnitTest // extends TestCase
         return $triple;
     }
 
-    /*
-     * Failed with the following error message: 
-     * Saft\Store\Test\AbstractTriplePatternStoreUnitTest::testAddStatements
-     * Expectation failed for method name is equal to <string:addStatements> when invoked 1 time(s).
-     * Method was expected to be called 1 times, actually called 0 times.
-     * 
-     * At this time, class Saft\Store\AbstractTriplePatternStore has no addStatements method....
-     *
     public function testAddStatements()
     {
         $triple = $this->getTestTriple();
         $quad = $this->getTestQuad();
         $query = 'INSERT DATA { '. $triple->toSparqlFormat(). $quad->toSparqlFormat(). '}';
-        $this->fixture->query($query);
         
         // Override abstract-methods: it will check the statements
         $this->fixture
-        ->expects($this->once())
-        ->method('addStatements')
-        ->will($this->returnCallback(
-            function (StatementIterator $statements, $graphUri = null, array $options = array()) {
-                PHPUnit_Framework_Assert::assertEquals(
-                    $statements[0]->toSparqlFormat(),
-                    $triple->toSparqlFormat()
-                );
-                PHPUnit_Framework_Assert::assertEquals(
-                    $statements[1]->toSparqlFormat(),
-                    $quad->toSparqlFormat()
-                );
-            }
-        ));
-    }*/
+            ->expects($this->once())
+            ->method('addStatements')
+            ->will(
+                $this->returnCallback(
+                    function (StatementIterator $statements, $graphUri = null, array $options = array()) {
+                        \PHPUnit_Framework_Assert::assertEquals(
+                            $statements->current()->toSparqlFormat(),
+                            $this->getTestTriple()->toSparqlFormat()
+                        );
+                        $statements->next();
+                        \PHPUnit_Framework_Assert::assertEquals(
+                            $statements->current()->toSparqlFormat(),
+                            $this->getTestQuad()->toSparqlFormat()
+                        );
+                    }
+                )
+            );
+        $this->fixture->query($query);
+    }
 
-    /*
-     * Failed with the following error message: 
-     * Saft\Store\Test\AbstractTriplePatternStoreUnitTest::testDeleteMatchingStatements
-     * Expectation failed for method name is equal to <string:deleteMatchingStatements> when invoked 1 time(s).
-     * Method was expected to be called 1 times, actually called 0 times.
-     * 
-     * At this time, class Saft\Store\AbstractTriplePatternStore has no deleteMatchingStatements method....
-     *
     public function testDeleteMatchingStatements()
     {
         $triple = $this->getTestTriple();
         $query = 'INSERT DATA { '.$triple->toSparqlFormat().'}';
-        $this->fixture->query($query);
         
         $this->fixture
             ->expects($this->once())
             ->method('deleteMatchingStatements')
-            ->will($this->returnCallback(
-                function (Statement $statement, $graphUri = null, array $options = array()) {
-                    PHPUnit_Framework_Assert::assertEquals(
-                        $statement->toSparqlFormat(),
-                        $triple->toSparqlFormat()
-                    );
-                }
-            ));
+            ->will(
+                $this->returnCallback(
+                    function (Statement $statement, $graphUri = null, array $options = array()) {
+                        \PHPUnit_Framework_Assert::assertEquals(
+                            $statement->toSparqlFormat(),
+                            $this->getTestTriple()->toSparqlFormat()
+                        );
+                    }
+                )
+            );
+        $this->fixture->delete($query);
     }
-    */
 }
