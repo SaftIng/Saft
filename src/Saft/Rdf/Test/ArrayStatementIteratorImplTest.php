1f3dc37b4b01c1344bcb5ffc827d7b88c800cec7
Natanael Arndt <arndtn@gmail.com>
2015-05-06 01:43:06 +0200

    Add test for StatementIterator

---
+++ src/Saft/Rdf/Test/ArrayStatementIteratorImplTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Saft\Rdf\Test;
+
+use \Saft\Rdf\ArrayStatementIteratorImpl;
+
+class ArrayStatementIteratorImplTest extends StatementIteratorAbstractTest
+{
+    public function createInstanceWithArray(array $statements)
+    {
+        return new ArrayStatementIteratorImpl($statements);
+    }
+}
---
+++ src/Saft/Rdf/Test/StatementIteratorAbstractTest.php
@@ -0,0 +1,68 @@
+<?php
+namespace Saft\Rdf\Test;
+
+use \Saft\Rdf\LiteralImpl;
+use \Saft\Rdf\NamedNodeImpl;
+use \Saft\Rdf\StatementImpl;
+use \Saft\Rdf\NodeFactory;
+use \Saft\TestCase;
+
+abstract class StatementIteratorAbstractTest extends TestCase
+{
+    abstract public function createInstanceWithArray(array $statements);
+
+    public function testIterationForeach()
+    {
+        $statements = [
+        new StatementImpl(
+            new NamedNodeImpl('http://s/'),
+            new NamedNodeImpl('http://p/'),
+            new NamedNodeImpl('http://o/')
+        ),
+        new StatementImpl(
+            new NamedNodeImpl('http://s/'),
+            new NamedNodeImpl('http://p/'),
+            new LiteralImpl('foobar', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#langString', 'en')
+        ),
+        new StatementImpl(
+            new NamedNodeImpl('http://s/'),
+            new NamedNodeImpl('http://p/'),
+            new LiteralImpl(42)
+        )];
+
+        $iterator = $this->createInstanceWithArray($statements);
+
+        $this->assertTrue($iterator->valid());
+
+        $actual = array();
+        foreach ($iterator as $key => $value) {
+            $actual[] = $value;
+        }
+
+        $this->assertEqualsArrays($statements, $actual);
+    }
+
+    public function testCountAssertion()
+    {
+        $statements = [
+        new StatementImpl(
+            new NamedNodeImpl('http://s/'),
+            new NamedNodeImpl('http://p/'),
+            new NamedNodeImpl('http://o/')
+        ),
+        new StatementImpl(
+            new NamedNodeImpl('http://s/'),
+            new NamedNodeImpl('http://p/'),
+            new LiteralImpl('foobar', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#langString', 'en')
+        ),
+        new StatementImpl(
+            new NamedNodeImpl('http://s/'),
+            new NamedNodeImpl('http://p/'),
+            new LiteralImpl(42)
+        )];
+
+        $iterator = $this->createInstanceWithArray($statements);
+
+        $this->assertCountStatementIterator(3, $iterator);
+    }
+}
