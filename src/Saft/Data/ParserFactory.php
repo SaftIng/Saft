20383b64e5ab4ce42bf44e8a8328cc0c374da2a5
Konrad Abicht <k.abicht@googlemail.com>
2015-05-21 15:05:01 +0200

    Correct missing $format > $serialization

--- src/Saft/Data/ParserFactory.php
+++ src/Saft/Data/ParserFactory.php
@@ -7,10 +7,10 @@ interface ParserFactory
     /**
      * Creates a Parser instance for a given format, if available.
      *
-     * @param  string     $format The format you need a parser for. In case it is not available, an exception
-     *                            will be thrown.
+     * @param  string     $serialization The serialization you need a parser for. In case it is not
+     *                                   available, an exception will be thrown.
      * @return Parser     Suitable parser for the requested format.
      * @throws \Exception If parser for requested format is not available.
      */
-    public function createParserFor($format);
+    public function createParserFor($serialization);
 }
