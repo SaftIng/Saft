278dcaf4546bc7358780344e731f829e5aeff749
Konrad Abicht <k.abicht@googlemail.com>
2015-06-01 10:49:04 +0200

    Redland: Skip NodeFactory related tests, if redland extension is not loaded

--- src/Saft/Backend/Redland/Test/Rdf/NodeFactoryTest.php
+++ src/Saft/Backend/Redland/Test/Rdf/NodeFactoryTest.php
@@ -7,12 +7,23 @@ use Saft\Backend\Redland\Rdf\NodeFactory;
 
 class NodeFactoryTest extends NodeFactoryAbstractTest
 {
-
     /**
      * An abstract method which returns new instances of NodeFactory
      */
     public function getFixture()
     {
         return new NodeFactory();
+    }
+
+    /**
+     *
+     */
+    public function setUp()
+    {
+        if (false === extension_loaded('redland')) {
+            $this->markTestSkipped('Extension redland is not loaded.');
+        }
+
+        parent::setUp();
     }
 }
