d8bef0c4ffbaa6a3b6b9dff1ec4cceb067e9bca1
Natanael Arndt <arndtn@gmail.com>
2015-06-26 13:00:26 +0200

    Throw Exception if NTriples string is not valid

--- src/Saft/Rdf/NodeFactory.php
+++ src/Saft/Rdf/NodeFactory.php
@@ -12,5 +12,11 @@ interface NodeFactory
 
     public function createAnyPattern();
 
+    /**
+     * Creates an RDF Node based on a N-Triples/N-Quads node string.
+     *
+     * @param $string string the N-Triples/N-Quads node string
+     * @throws \Exception if no node could be created e.g. because of a syntax error in the node string
+     */
     public function createNodeFromNQuads($string);
 }
