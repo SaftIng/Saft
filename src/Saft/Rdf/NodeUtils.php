b5e79d9fe2e2186109fa8e8f49c1fe5b4d666dec
Konrad Abicht <k.abicht@googlemail.com>
2015-06-30 18:11:43 +0200

    NodeUtils: add function createNodeInstance
    That functions helps you to save code. It is useful in
    cases where you have all the meta information about a
    node (type, value, ...) but dont want to write that
    switch to create the right node type, especially if you
    want to use a NodeFactory too.
    
    Tests also added.

--- src/Saft/Rdf/NodeUtils.php
+++ src/Saft/Rdf/NodeUtils.php
@@ -5,6 +5,39 @@ namespace Saft\Rdf;
 class NodeUtils
 {
     /**
+     * @param NodeFactory $nodeFactory Instance of the NodeFactory to use.
+     * @param string      $value       Value of the node.
+     * @param string      $type        Can be uri, bnode, var or literal
+     * @param string      $datatype    URI of the datatype (optional)
+     * @param string      $language    Language tag (optional)
+     * @return Node Node instance
+     */
+    public static function createNodeInstance(
+        NodeFactory $nodeFactory,
+        $value,
+        $type,
+        $datatype = null,
+        $language = null
+    ) {
+        switch ($type) {
+            case 'uri':
+                return $nodeFactory->createNamedNode($value);
+
+            case 'bnode':
+                return $nodeFactory->createBlankNode($value);
+
+            case 'literal':
+                return $nodeFactory->createLiteral($value, $datatype, $language);
+
+            case 'var':
+                return $nodeFactory->createAnyPattern();
+
+            default:
+                throw new \Exception('Unknown $type given: '. $type);
+        }
+    }
+
+    /**
      * Checks the general syntax of a given URI. Protocol-specific syntaxes are
      * not checked. Instead, only characters disallowed an all URIs lead to a
      * rejection of the check.
--- src/Saft/Rdf/Test/NodeUtilsTest.php
+++ src/Saft/Rdf/Test/NodeUtilsTest.php
@@ -2,11 +2,65 @@
 
 namespace Saft\Rdf\Test;
 
+use Saft\Rdf\BlankNodeImpl;
+use Saft\Rdf\LiteralImpl;
+use Saft\Rdf\NamedNodeImpl;
+use Saft\Rdf\NodeFactoryImpl;
 use Saft\Rdf\NodeUtils;
 use Saft\Test\TestCase;
 
 class NodeUtilsTest extends TestCase
 {
+    /*
+     * Tests for createNodeInstance
+     */
+
+    public function testCreateNodeInstanceBNode()
+    {
+        $node = NodeUtils::createNodeInstance(
+            new NodeFactoryImpl(),
+            'bid',
+            'bnode'
+        );
+        
+        $this->assertEquals(new BlankNodeImpl('bid'), $node);        
+    }
+    
+    public function testCreateNodeInstanceLiteral()
+    {
+        $node = NodeUtils::createNodeInstance(
+            new NodeFactoryImpl(),
+            '42',
+            'literal',
+            'xsd:int'
+        );
+        
+        $this->assertEquals(new LiteralImpl('42', new NamedNodeImpl('xsd:int')), $node);        
+    }
+    
+    public function testCreateNodeInstanceUnknown()
+    {
+        // expect exception, because given type is unknown
+        $this->setExpectedException('\Exception');
+        
+        $node = NodeUtils::createNodeInstance(
+            new NodeFactoryImpl(),
+            null,
+            'unknown'
+        );
+    }
+    
+    public function testCreateNodeInstanceUri()
+    {
+        $node = NodeUtils::createNodeInstance(
+            new NodeFactoryImpl(),
+            'http://foo',
+            'uri'
+        );
+        
+        $this->assertEquals(new NamedNodeImpl('http://foo'), $node);
+    }
+    
     /*
      * Tests for simpleCheckURI
      */
