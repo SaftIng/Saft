984be495163903fd93e3b5e22a4dd203b3472e22
Konrad Abicht <k.abicht@googlemail.com>
2015-06-30 18:42:10 +0200

    Move tests for Result classes to Saft/Sparql
    Because they were missed.
    
    Added EmptyResultImplTest.php with basic tests.

---
+++ src/Saft/Sparql/Test/Result/EmptyResultImplTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Saft\Sparql\Test\Result;
+
+use Saft\Sparql\Result\EmptyResultImpl;
+use Saft\Test\TestCase;
+
+class EmptyResultImplTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->fixture = new EmptyResultImpl();
+    }
+
+    /*
+     * Tests for isEmptyResult
+     */
+
+    public function testIsEmptyResult()
+    {
+        $this->assertTrue($this->fixture->isEmptyResult());
+    }
+    
+    /*
+     * Tests for isSetResult
+     */
+
+    public function testIsSetResult()
+    {
+        $this->assertFalse($this->fixture->isSetResult());
+    }
+    
+    /*
+     * Tests for isStatementSetResult
+     */
+
+    public function testIsStatementSetResult()
+    {
+        $this->assertFalse($this->fixture->isStatementSetResult());
+    }
+    
+    /*
+     * Tests for isValueResult
+     */
+
+    public function testIsValueResult()
+    {
+        $this->assertFalse($this->fixture->isValueResult());
+    }
+}
