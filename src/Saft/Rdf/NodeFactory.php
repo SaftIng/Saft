c5484d332dc7dd3007802b30518f822e6bd9f843
Konrad Abicht <k.abicht@googlemail.com>
2015-07-06 11:25:38 +0200

    Improve documentation for NodeFactory and NodeUtils
    Made it part of the API (@api) and added them to
    package Saft\Rdf.

--- src/Saft/Rdf/NodeFactory.php
+++ src/Saft/Rdf/NodeFactory.php
@@ -5,47 +5,53 @@ namespace Saft\Rdf;
 /**
  * The NodeFactory interface abstracts the creating of new instances of RDF nodes by hiding different implementation
  * details.
+ *
+ * @api
+ * @package Saft\Rdf
  */
 interface NodeFactory
 {
     /**
-     * Create a new RDF Literal Node
+     * Create a new RDF literal node instance. Details how to create such an instance may differ between different
+     * implementations of the NodeFactory.
      *
      * @param string $value The value of the literal
      * @param string|Node $datatype The datatype of the literal (NamedNode, optional)
      * @param string $lang The language tag of the literal (optional)
-     * @return Literal
+     * @return Literal Instance of Literal.
      */
     public function createLiteral($value, $datatype = null, $lang = null);
 
     /**
-     * Create a new RDF Named Node
+     * Create a new RDF named node. Details how to create such an instance may differ between different
+     * implementations of the NodeFactory.
      *
-     * @param string $uri The URI of the Named Node
-     * @return NamedNode
+     * @param string $uri The URI of the named node
+     * @return NamedNode Instance of NamedNode.
      */
     public function createNamedNode($uri);
 
     /**
-     * Create a new RDF Blank Node
+     * Create a new RDF blank node. Details how to create such an instance may differ between different
+     * implementations of the NodeFactory.
      *
      * @param string $blankId The identifier for the blank node
-     * @return BlankNode
+     * @return BlankNode Instance of BlankNode.
      */
     public function createBlankNode($blankId);
 
     /**
-     * Create a new pattern node, which matches any RDF Node
+     * Create a new pattern node, which matches any RDF Node instance.
      *
-     * @return Node
+     * @return Node Instance of Node, which acts like an AnyPattern.
      */
     public function createAnyPattern();
 
     /**
-     * Creates an RDF Node based on a N-Triples/N-Quads node string.
+     * Creates an RDF Node based on a n-triples/n-quads node string.
      *
-     * @param string $string the N-Triples/N-Quads node string
-     * @return Node
+     * @param string $string N-triples/n-quads node string to use.
+     * @return Node Node instance, which type must be one of the following: NamedNode, BlankNode, Literal
      * @throws \Exception if no node could be created e.g. because of a syntax error in the node string
      */
     public function createNodeFromNQuads($string);
--- src/Saft/Rdf/NodeUtils.php
+++ src/Saft/Rdf/NodeUtils.php
@@ -2,15 +2,26 @@
 
 namespace Saft\Rdf;
 
+/**
+ * Helper class which provides useful methods for Node related operations, for instance node creation or URI
+ * checks.
+ *
+ * @api
+ * @package Saft\Rdf
+ */
 class NodeUtils
 {
     /**
+     * Helper function which is useful, if you have all the meta information about a Node and want to create
+     * the according Node instance. It utilizes a NodeFactory instance to create Node instances.
+     *
      * @param NodeFactory $nodeFactory Instance of the NodeFactory to use.
      * @param string      $value       Value of the node.
      * @param string      $type        Can be uri, bnode, var or literal
      * @param string      $datatype    URI of the datatype (optional)
      * @param string      $language    Language tag (optional)
-     * @return Node Node instance
+     * @return Node Node instance, which type is one of: NamedNode, BlankNode, Literal, AnyPattern
+     * @throws \Exception if an unknown type was given.
      */
     public static function createNodeInstance(
         NodeFactory $nodeFactory,
@@ -38,9 +49,10 @@ class NodeUtils
     }
 
     /**
-     * Checks the general syntax of a given URI. Protocol-specific syntaxes are
-     * not checked. Instead, only characters disallowed an all URIs lead to a
-     * rejection of the check.
+     * Checks the general syntax of a given URI. Protocol-specific syntaxes are not checked. Instead, only
+     * characters disallowed an all URIs lead to a rejection of the check. Use this function, if you need a
+     * basic check and if performance is an issuse. In case you need a more precise check, that function is
+     * not recommended.
      *
      * @param string $string String to check if its a URI or not.
      * @return boolean True if given string is a valid URI, false otherwise.
