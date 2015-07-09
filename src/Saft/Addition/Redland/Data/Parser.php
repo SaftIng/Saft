bfaea1a181ca142c2796c0edeea733a829bc69d0
Konrad Abicht <k.abicht@googlemail.com>
2015-07-09 14:37:09 +0200

    add further code-documentation to Redland parser

--- src/Saft/Addition/Redland/Data/Parser.php
+++ src/Saft/Addition/Redland/Data/Parser.php
@@ -24,6 +24,11 @@ class Parser implements ParserInterface
      */
     protected $parser;
 
+    /**
+     * Constructor.
+     *
+     * @param string $serialization Serialization term to use.
+     */
     public function __construct($serialization)
     {
         if (!extension_loaded('redland')) {
@@ -39,10 +44,15 @@ class Parser implements ParserInterface
     }
 
     /**
-     * @param $inputString
-     * @param $baseUri
-     * @return StatementIterator
-     * @throws Exception
+     * Parses a given string and returns an iterator containing Statement instances representing the
+     * previously read data.
+     *
+     * @param string $inputString Data string containing RDF serialized data.
+     * @param string $baseUri     The base URI of the parsed content. If this URI is null the inputStreams URL
+     *                            is taken as base URI.
+     * @return StatementIterator StatementIterator instaince containing all the Statements parsed by the
+     *                           parser to far.
+     * @throws \Exception If the base URI $baseUri is no valid URI.
      */
     public function parseStringToIterator($inputString, $baseUri = null)
     {
@@ -55,10 +65,14 @@ class Parser implements ParserInterface
     }
 
     /**
-     * @param $inputStream
-     * @param $baseUri
-     * @return StatementIterator
-     * @throws Exception
+     * Parses a given stream and returns an iterator containing Statement instances representing the
+     * previously read data. The stream parses the data not as a whole but in chunks.
+     *
+     * @param string $inputStream Filename of the stream to parse which contains RDF serialized data.
+     * @param string $baseUri     The base URI of the parsed content. If this URI is null, the inputStreams URL is taken
+     *                            as base URI. (optional)
+     * @return StatementIterator A StatementIterator containing all the Statements parsed by the parser to far.
+     * @throws \Exception if creation of librdf_uri from the given $baseUri failed.
      */
     public function parseStreamToIterator($inputStream, $baseUri = null)
     {
@@ -73,6 +87,13 @@ class Parser implements ParserInterface
         return $this->parseStringToIterator($data, $baseUri, $serialization);
     }
 
+    /**
+     * Returns an array of prefixes which where found during the last parsing. Might also be any other prefix list
+     * depending on the implementation. Might even be empty.
+     *
+     * @return array An associative array with a prefix mapping of the prefixes parsed so far. The key
+     *               will be the prefix, while the values contains the according namespace URI.
+     */
     public function getCurrentPrefixlist()
     {
         $prefixCount = count($this->prefixes);
