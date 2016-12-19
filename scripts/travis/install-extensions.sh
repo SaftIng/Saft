c3ae46aba3b292229c92b22d7b7baf5f05ca1b69
Natanael Arndt <arndtn@gmail.com>
2016-12-19 17:23:23 +0100

    add docize

--- scripts/travis/install-extensions.sh
+++ scripts/travis/install-extensions.sh
@@ -46,8 +46,8 @@ php -m
 
 # build redland
 cd ../../../
-# swig is required by the redland-bindings, bison is required to build raptor and rasqal
-sudo apt-get -y install swig bison #gtk-doc-tools # gtk-doc-tools and #librasqal3-dev
+# swig is required by the redland-bindings, gtk-doc-tools is required by raptor and librdf, bison is required to build raptor and rasqal
+sudo apt-get -y install swig bison gtk-doc-tools #librasqal3-dev
 git clone git://github.com/dajobe/rasqal.git
 git clone git://github.com/dajobe/raptor.git
 git clone git://github.com/dajobe/librdf.git
